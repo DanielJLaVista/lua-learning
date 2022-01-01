7b18ef30b8bf6891d9cf8cb84e6a943f0741d0b1
DanielJLaVista <daniel.j.lavista@gmail.com>
2022-01-02 00:42:19 +1030

    Learning functions

--- Main.lua
+++ Main.lua
@@ -3,3 +3,21 @@ print("What is your name Player?")
 local player = io.read()
 
 print("Welcome to the game " .. player)
+
+local function printHelloWorld()
+    print("Hello world")
+end
+
+printHelloWorld()
+
+function GetValues()
+    return 1,2
+end
+
+local val1, val2, val3 = GetValues()
+print(val1 .. " | " .. val2)
+print(val3)
+
+local function functionWithMultipleArgs(...)
+
+end
